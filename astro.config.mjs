e062f36739aae7243b1e0463df4483d9f6aefff5
Jonathan <jnbns42@gmail.com>
2026-01-05 09:27:18 +0000

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jnbns42.github.io/',
+    site: 'https://www.jbaines.dev',
 });
